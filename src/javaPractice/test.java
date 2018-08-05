9730bf5e102df2b2c53279273998f286fa96529e
shilpisirohi12 <shilpisirohi12@gmail.com>
2018-08-06 01:40:03 +0530

    hello world example

---
+++ src/javaPractice/test.java
@@ -0,0 +1,14 @@
+package javaPractice;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello  World");
+	}
+
+}
